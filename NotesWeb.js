ae05d2e85b62b67f07149ccf0d8c4798bcff7912
Khushi Gupta <60961722+khushi138@users.noreply.github.com>
2022-04-01 12:25:49 +0530

    Update NoteWeb.js

--- NotesWeb.js
+++ NotesWeb.js
@@ -1,24 +1,71 @@
-console.log("hello peeps");
-let addBtn = document.getElementById("addBtn").addEventListener('click', function(e) {
-    let addText = document.getElementById("addText");
-    let notes = localStorage.getItem("notes");
+
+display();
+displayBookmark();
+let text = document.getElementById("Textarea");
+let addBtn = document.getElementById("addBtn");
+
+addBtn.addEventListener('click', function addNotes() {
+
+    let notes = localStorage.getItem("yourNotes");
     if (notes == null) {
-        noteObj = []
+        noteObj = [];
     }
     else {
         noteObj = JSON.parse(notes);
     }
-    noteObj.push(addText.value);
-    localStorage.setItem("notes", JSON.stringify(noteObj));
-    addText.value = "";
-    console.log(noteObj);
+    noteObj.push(text.value);
+    localStorage.setItem("yourNotes", JSON.stringify(noteObj));
+    text.value = "";
+    // console.log(noteObj);
 
-    showNotes();
-})
+    display();
+});
 
-//function to show elements from local storage
-function showNotes() {
-    let notes = localStorage.getItem("notes");
+let impBtn = document.getElementById("impBtn");
+impBtn.addEventListener('click', function bookmarkShow() {
+    let booknotes = localStorage.getItem("bookNotes");
+    if (booknotes == null) {
+        bookmarkObj = [];
+    }
+    else {
+        bookmarkObj = JSON.parse(booknotes);
+    }
+    bookmarkObj.push(text.value);
+    localStorage.setItem("bookNotes", JSON.stringify(bookmarkObj));
+    text.value = "";
+
+    displayBookmark();
+});
+
+function displayBookmark() {
+    booknotes = localStorage.getItem("bookNotes");
+    if (booknotes == null) {
+        bookmarkObj = [];
+    }
+    else {
+        bookmarkObj = JSON.parse(booknotes);
+    }
+    let html = "";
+    bookmarkObj.forEach(function (element, index) {
+        html += `
+        <div class="card text-dark border-dark mb-3 my-2 mx-2  shadow p-3 mb-5  rounded impColor" style="max-width: 18rem;">
+                
+                <div class="card-body impColor">
+                    <h5 class="card-title">Imp note</h5>
+                    <p class="card-text">${element}</p>
+                    <button  class="btn btn-primary" id="${index}"onclick="deleteBookmark(this.id)">Delete</button>
+                </div>
+                
+               
+            </div>`
+    });
+    let cardBookmark = document.getElementById("bookmark");
+    cardBookmark.innerHTML = html;
+
+}
+function display() {
+
+    let notes = localStorage.getItem("yourNotes");
     if (notes == null) {
         noteObj = [];
     }
@@ -28,55 +75,48 @@ function showNotes() {
     let html = "";
     noteObj.forEach(function (element, index) {
         html += `
-                <div class="card my-2 mx-2 noteCard" style="width: 18rem;">
-
-                    <div class="card-body">
-                        <h5 class="card-title">Note ${index + 1}</h5>
-                        <p class="card-text"> ${element}</p>
-                        <button id="${index}" onClick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
-                    </div>
-                </div>`;
+        <div class="card text-dark border-dark mb-3 my-2 mx-2  shadow p-3 mb-5 bg-white rounded" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">Note${index + 1}</h5>
+            <p class="card-text">${element}</p>
+            <button class="btn btn-primary" id="${index}"onclick="deleteNote(this.id)">Delete</button>
+        </div>
+            </div>`
     });
-   
-    let noteElm = document.getElementById("notes");
-    if (noteObj.length != 0) {
-        noteElm.innerHTML = html;
-    } else {
-        noteElm.innerHTML = `Nothng to show! Use 'Add Note' section above to add notes `;
-    }
-
+    let card = document.getElementById("card-container");
+    card.innerHTML = html;
 }
 
-// function to delete a note
 
-function deleteNote(index){
-    console.log("I'm deleting",index);
-    let notes = localStorage.getItem("notes");
+function deleteNote(index) {
+    let notes = localStorage.getItem("yourNotes");
     if (notes == null) {
         noteObj = [];
     }
     else {
         noteObj = JSON.parse(notes);
     }
-    noteObj.splice(index, 1); //removes 1 element from position index
-    localStorage.setItem("notes", JSON.stringify(noteObj));
-    showNotes();
-
+    noteObj.splice(index, 1);
+    localStorage.setItem("yourNotes", JSON.stringify(noteObj));
+    display();
+}
+function deleteBookmark(index) {
+    let booknotes = localStorage.getItem("bookNotes");
+    if (booknotes == null) {
+        bookmarkObj = [];
+    }
+    else {
+        bookmarkObj = JSON.parse(booknotes);
+    }
+    bookmarkObj.splice(index, 1);
+    localStorage.setItem("bookNotes", JSON.stringify(bookmarkObj));
+    displayBookmark();
 }
 
-let search = document.getElementById("searchTxt");
-search.addEventListener("input", function(){
 
-    let inputval = search.value.toLowerCase();
-    console.log("input event fired!", inputval);
-    let noteCard = document.getElementsByClassName("noteCard");
-    Array.from(noteCard).forEach(function(element){
-        let cardTxt = document.getElementsByTagName("p")[0].innerText;
-        console.log(cardTxt);
-        if(cardTxt.includes(inputval)){
-            element.style.display = "block";
-        } else{
-            element.style.display = "none";
-        }
-    })
-})
+
+
+
+
+
+
